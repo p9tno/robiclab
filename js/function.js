d5a7b7bf3cbb15c1097dc30930566369ed662ded
p9tno <plahotin_s@mail.ru>
2023-01-25 09:51:53 +0300

    start pages

--- js/function.js
+++ js/function.js
@@ -144,6 +144,30 @@ $(document).ready(function() {
     showModal();
 
 
+    function changeTheme() {
+        $('input[name=themeCheckbox]').change(function() {
+
+            if ($(this).is(':checked')) {
+                // console.log("Checkbox is checked..");
+                localStorage.setItem("theme", "night");
+                // console.log(localStorage.getItem("theme"));
+                $('input[name=themeCheckbox]').prop('checked', true);
+            } else {
+                // console.log("Checkbox is not checked..");
+                localStorage.setItem("theme", "day");
+                $('input[name=themeCheckbox]').prop('checked', false);
+            }
+            $( 'body' ).toggleClass('dark_theme');
+        });
+
+        if (localStorage.getItem("theme") == "night") {
+            $('input[name=themeCheckbox]').prop('checked', true);
+            // body.className += " dark-theme";
+            $( 'body' ).toggleClass('dark_theme');
+        }
+    }
+    changeTheme();
+
     // scrollTop
     // $(document).ready(function(){
     //     //отменяем стандартную обработку нажатия по ссылке
